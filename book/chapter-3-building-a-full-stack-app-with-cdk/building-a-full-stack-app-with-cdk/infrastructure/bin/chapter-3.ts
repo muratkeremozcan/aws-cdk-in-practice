7a1ca89cc67784c12644016ff9d67b3d316a442e
Murat Ozcan <muratkerem@gmail.com>
2023-08-08 09:34:57 -0500

    ch3 done [ci skip]

--- book/chapter-3-building-a-full-stack-app-with-cdk/building-a-full-stack-app-with-cdk/infrastructure/bin/chapter-3.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-
-import { Chapter3Stack } from '../lib/chapter-3-stack';
-
-const app = new cdk.App();
-
-new Chapter3Stack(app, 'Chapter3Stack', {});
