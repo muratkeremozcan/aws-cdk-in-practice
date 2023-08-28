29bee9158cbf7bd0f6492b621aa94d46868b3ad6
Murat Ozcan <muratkerem@gmail.com>
2023-08-28 13:40:47 -0500

    added frontend test

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -2,13 +2,17 @@ import {defineConfig} from 'cypress'
 import tasks from './cypress/support/tasks'
 import {getEnvironmentConfig} from '../infrastructure/lib/get-env-config'
 import {domain_name} from '../config.json'
+import {initCredentials} from './cypress/support/init-credentials'
+import {getBaseUrl} from './cypress/support/get-base-url'
+import AWS from 'aws-sdk'
+
 require('dotenv').config()
 
-const {backend_subdomain, frontend_subdomain} = getEnvironmentConfig(
-  process.env.NODE_ENV!,
+const {backend_subdomain, deployment} = getEnvironmentConfig(
+  process.env.NODE_ENV,
 )
 const apiUrl = `https://${backend_subdomain}.${domain_name}`
-const baseUrl = `https://${frontend_subdomain}.${domain_name}`
+console.log({deployment, apiUrl})
 
 export default defineConfig({
   viewportWidth: 1380,
@@ -19,15 +23,35 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl,
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
+      const awsConfig = await initCredentials()
+
+      if (awsConfig) {
+        AWS.config.update({
+          accessKeyId: awsConfig.credentials.accessKeyId,
+          secretAccessKey: awsConfig.credentials.secretAccessKey,
+          sessionToken: awsConfig.credentials.sessionToken,
+          region: awsConfig.region,
+        })
+      } else {
+        console.error('Could not initialize AWS credentials')
+      }
+
+      const baseUrl = await getBaseUrl(deployment)
+      if (baseUrl) {
+        config.baseUrl = baseUrl
+      } else {
+        console.error('Could not get base URL')
+      }
+
       tasks(on)
 
       return config
     },
   },
   env: {
-    apiUrl,
     ...process.env,
+    apiUrl,
+    ENVIRONMENT: deployment,
   },
 })
--- web/cypress/e2e/crud-fe.cy.ts
+++ web/cypress/e2e/crud-fe.cy.ts
@@ -1,3 +1,6 @@
+import spok from 'cy-spok'
+import 'cypress-map'
+
 type Todo = {
   id?: string
   todo_name: string
@@ -14,7 +17,67 @@ describe('CRUD', () => {
   it('should crud the front end', () => {
     cy.visit('/')
 
+    // TODO add backend assertions
+    cy.intercept('POST', '*').as('post')
+    cy.intercept('GET', '*').as('get')
+    cy.intercept('PUT', '*').as('put')
+    cy.intercept('DELETE', '*').as('delete')
+
     cy.get('[placeholder="name"]').type(todo.todo_name)
     cy.get('[placeholder="description"]').type(todo.todo_description)
+    cy.contains('button', 'Add').click()
+    cy.wait('@post')
+      .its('response')
+      .should(
+        spok({
+          statusCode: 200,
+          body: {
+            todo,
+          },
+        }),
+      )
+
+    const getTodoBtn = (button: 'Delete' | 'Edit') =>
+      cy
+        .contains('h1', todo.todo_name)
+        .should('be.visible')
+        .parent()
+        .siblings()
+        .children()
+        .contains(button)
+
+    getTodoBtn('Edit').click()
+    const editedTodoDescription = String(`edited-${Cypress._.random(1000)}`)
+    cy.get(`[value="${todo.todo_description}"]`)
+      .clear()
+      .type(editedTodoDescription, {delay: 0})
+      .siblings()
+      .contains('Save')
+      .click()
+    cy.wait('@put')
+      .its('response')
+      .should(
+        spok({
+          statusCode: 200,
+          body: {
+            todo: {
+              ...todo,
+              todo_description: editedTodoDescription,
+            },
+          },
+        }),
+      )
+
+    getTodoBtn('Delete').click()
+    cy.wait('@delete')
+      .its('response')
+      .should(
+        spok({
+          statusCode: 200,
+          body: {
+            message: 'Todo deleted successfully.',
+          },
+        }),
+      )
   })
 })
