774c463bba9c1b31b07616ff1ef4a6e9cd407c21
Murat Ozcan <muratkerem@gmail.com>
2023-09-01 11:56:36 -0500

    test using .env for everythig

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -1,16 +1,8 @@
 import {defineConfig} from 'cypress'
 import tasks from './cypress/support/tasks'
-import {getEnvironmentConfig} from '../infrastructure/lib/get-env-config'
-import {domain_name} from '../config.json'
 import {configAWSForLocal} from './cypress/support/config-aws'
 
 require('dotenv').config()
-
-const {backend_subdomain, deployment} = getEnvironmentConfig(
-  process.env.NODE_ENV || 'dev',
-)
-const apiUrl = `https://${backend_subdomain}.${domain_name}`
-console.log({deployment, apiUrl})
 
 export default defineConfig({
   viewportWidth: 1380,
@@ -23,7 +15,7 @@ export default defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       if (!process.env.CI) {
-        await configAWSForLocal(config, deployment)
+        await configAWSForLocal(config, process.env.ENVIRONMENT)
       }
 
       tasks(on)
@@ -33,7 +25,6 @@ export default defineConfig({
   },
   env: {
     ...process.env,
-    apiUrl,
-    ENVIRONMENT: deployment,
+    ENVIRONMENT: process.env.ENVIRONMENT,
   },
 })
--- web/get-backend-url.js
+++ web/get-backend-url.js
@@ -1,13 +1,27 @@
 const fs = require('fs')
-const {
-  getEnvironmentConfig,
-} = require('../infrastructure/dist/infrastructure/lib/get-env-config.js')
-const config = require('../config.json')
+const path = require('path')
 
-const {backend_subdomain} = getEnvironmentConfig(process.env.NODE_ENV || 'dev')
-const backendURL = `https://${backend_subdomain}.${config.domain_name}`
+const envFilePath = path.resolve(__dirname, '../infrastructure/.env')
+
+// Read the .env file and convert it to an object
+const envFileContent = fs.readFileSync(envFilePath, 'utf-8')
+const envVars = envFileContent.split('\n').reduce((acc, line) => {
+  const [key, value] = line.split('=')
+  acc[key] = value
+  return acc
+}, {})
+
+// Get the ApiGatewayUrl and deployment from the .env file
+const backendURL = envVars.ApiGatewayUrl
+const deployment = envVars.deployment
+console.log(`Using backend URL: ${backendURL}`)
+console.log(`Using ENVIRONMENT: ${deployment}`)
 
 // Write to .env file
-fs.writeFileSync('.env', `REACT_APP_BACKEND_URL=${backendURL}\n`)
+fs.writeFileSync(
+  '.env',
+  `REACT_APP_BACKEND_URL=${backendURL}\nENVIRONMENT=${deployment}\n`,
+)
 
 console.log(`Configured for backend URL: ${backendURL}`)
+console.log(`Configured for ENVIRONMENT: ${deployment}`)
